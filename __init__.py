9e1c607d5306bb7b1a9d200a202c501541667d16
unknown <tuonomindcode@bahnhof.se>
2026-01-16 21:47:28 +0100

    v1.1.1: Fix Main Simple node registration in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,19 @@
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 from .sequence_prompt_nodes import (
     MultiClipTextScriptMain,
     MultiClipTextScriptClipSelector,
+    MultiClipTextScriptPositiveOnly,
 )
 
 NODE_CLASS_MAPPINGS = {
     "MultiClipTextScriptMain": MultiClipTextScriptMain,
     "MultiClipTextScriptClipSelector": MultiClipTextScriptClipSelector,
+    "MultiClipTextScriptPositiveOnly": MultiClipTextScriptPositiveOnly,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "MultiClipTextScriptMain": "Multi-Clip Text Script: Main",
     "MultiClipTextScriptClipSelector": "Multi-Clip Text Script: Clip Selector",
+    "MultiClipTextScriptPositiveOnly": "Multi-Clip Text Script: Main Simple",
 }
